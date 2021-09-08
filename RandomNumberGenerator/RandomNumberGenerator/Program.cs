e4452d7a72ba5c70c65834e605021b290fab8466
Opilane TTHK <opilane@tthk.ee>
2021-09-08 12:13:50 +0300

    randomnumbers

---
+++ RandomNumberGenerator/RandomNumberGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+
+            int mynumber = rnd.Next(1, 11);
+
+            //programm genereerib juhuslikku numbrit 1 kuni 10
+            //programm kontrollib kas genereeritud number on suurem kui 5
+            //kui number on suurem kui 5, siis konsool kuvab "juhuslik number on {mynumber}, see on suurem kui 5";
+            //kui number on väiksem kui 5, konsool kuvab "juhuslik number on {mynumber}, see on väiksem kui 5";
+            //kui number on 5, konsool kuvab "juhuslik number on 5";
+
+            if (mynumber > 5)
+            {
+                Console.WriteLine("juhuslik number on " + mynumber + ", see on suurem kui 5");
+            }
+
+            else if (mynumber < 5)
+            {
+                Console.WriteLine("juhuslik number on " + mynumber + ", see on väiksem kui 5");
+            }
+
+            else
+            {
+                Console.WriteLine("juhuslik number on " + mynumber);
+            }
+
+            
+
+
+           
+
+
+
+
+        }
+    }
+}
---
+++ TäringuMäng/RockPaperScissors/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TäringuMäng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string esimenemängija = "Guacamoleniggapenis";
+            int plr1 = rnd.Next(1, 7);
+
+            Random rnd2 = new Random();
+            string teinemängija = "banaan";
+            int plr2 = rnd.Next(1, 7);
+
+            Console.WriteLine(esimenemängija + " viskas täringuga numbri " + plr1);
+            Console.WriteLine(teinemängija + " viskas täringuga numbri " + plr2);
+
+            if (plr1 < plr2)
+            {
+                Console.WriteLine(esimenemängija + " võitis!");
+            }
+            else if (plr1 > plr2)
+            {
+                Console.WriteLine(teinemängija + " võitis!");
+            }
+            else
+            {
+                Console.WriteLine("VIIK!");
+            }
+
+        }
+    }
+}
